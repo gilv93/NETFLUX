f80ab60d9113b2ca50f450c46c98f728ff202cc2
Gilbert Velazquez <gv2793@gmail.com>
2019-10-07 12:12:18 -0600

    Updates

---
+++ precache-manifest.e80c48d48b50607c349d24f478786ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775d6845493e68a26cb9d7507b50d091",
+    "url": "/NETFLUX/index.html"
+  },
+  {
+    "revision": "f98d0d4c9b18369aaad3",
+    "url": "/NETFLUX/static/css/main.613de3fe.chunk.css"
+  },
+  {
+    "revision": "7b8a789fefccee541d2b",
+    "url": "/NETFLUX/static/js/2.87f89412.chunk.js"
+  },
+  {
+    "revision": "f98d0d4c9b18369aaad3",
+    "url": "/NETFLUX/static/js/main.f1a0499e.chunk.js"
+  },
+  {
+    "revision": "46c52ff1c2e2e18e82a8",
+    "url": "/NETFLUX/static/js/runtime~main.27a9e751.js"
+  }
+]);
